1d8eedec7efd1be517b2953786901c9e762daf00
Rahul Wagh <143119467+imrahulwagh@users.noreply.github.com>
2026-02-23 11:37:22 +0530

    Challenge 21 done

---
+++ CH21.java
@@ -0,0 +1,18 @@
+// 21. Create a program that shows bitwise AND of two numbers.
+package Practice;
+
+import java.util.Scanner;
+
+public class CH21 {
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the first number : ");
+		int no1 = sc.nextInt();
+		System.out.print("Enter the first number : ");
+		int no2 = sc.nextInt();
+		int res = no1 & no2;
+		System.out.println(res);
+		sc.close();
+	}
+}
