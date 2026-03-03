17bcaa68916162fe260a68f44e4271e92027d3af
Rahul Wagh <143119467+imrahulwagh@users.noreply.github.com>
2026-03-03 22:54:27 +0530

    Refactor CH40 to use ArrayUtility for input

--- src/CH40.java
+++ src/CH40.java
@@ -1,21 +1,14 @@
 // 40. Create a program to find the sum and average of all elements in an array.
-import java.util.Scanner;
 public class CH40 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number : ");
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        System.out.print("Enter "+n+" elements : ");
+        int[] arr = ArrayUtility.inputArray();
         int i=0,sum=0;
         while(i<arr.length)
         {
-            arr[i] = sc.nextInt();
             sum += arr[i];
             i++;
         }
         System.out.println("Sum : "+sum);
-        System.out.println("Average : "+(float)sum/n);
-        sc.close();
+        System.out.println("Average : "+(float)sum/arr.length);
     }
 }
