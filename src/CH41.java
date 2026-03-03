a2d92d5936d1ed61b27c5804cadf674d1fbece9e
Rahul Wagh <143119467+imrahulwagh@users.noreply.github.com>
2026-03-03 23:02:47 +0530

    Refactor occurrence counting into a separate method

--- src/CH41.java
+++ src/CH41.java
@@ -2,30 +2,25 @@
 import java.util.Scanner;
 
 public class CH41 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number : ");
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        System.out.print("Enter "+n+" elements : ");
-        int i=0,count= 0;
+    public static int occurrenceInArray(int[] arr, int key)
+    {
+        int i=0,count=0;
         while(i<arr.length)
         {
-            arr[i] = sc.nextInt();
-            i++;
-        }
-        System.out.print("Enter a number for occurrence check : ");
-        int occ_no = sc.nextInt();
-        int j = 0;
-        while(j<arr.length)
-        {
-            if(arr[j] == occ_no)
+            if(arr[i]==key)
             {
                 count++;
             }
-            j++;
+            i++;
         }
-        System.out.println("Occurrence of "+occ_no+" is "+count);
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = ArrayUtility.inputArray();
+        System.out.print("Enter a number for occurrence check : ");
+        int key = sc.nextInt();
+        System.out.println("Occurrence of "+key+" is "+ occurrenceInArray(arr,key));
         sc.close();
     }
 }
