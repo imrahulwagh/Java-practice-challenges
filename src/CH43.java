ba237560944f2b3f36fdb6419885732b84180956
Rahul Wagh <143119467+imrahulwagh@users.noreply.github.com>
2026-03-03 23:22:15 +0530

    Refactor array sorting check with utility methods

--- src/CH43.java
+++ src/CH43.java
@@ -1,41 +1,40 @@
 // 43. Create a program to check if the given array is sorted
 import java.util.Scanner;
 public class CH43 {
-    static Scanner sc = new Scanner(System.in);
-    public static void inputArray(int[] arr, int n)
-    {
-        System.out.print("Enter "+n+" elements : ");
-        int i=0,count= 0;
-        while(i<arr.length)
-        {
-            arr[i] = sc.nextInt();
-            i++;
-        }
-    }
-    // is array sorted
-    public static boolean isSorted(int[] arr)
-    {
-        int i = 0;
-        while(i<arr.length-1)
-        {
-            if(arr[i]>arr[i+1])
-                return false;
-            i++;
-        }
-        return true;
-    }
+
     public static void main(String[] args) {
-        System.out.print("Enter a array size : ");
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        inputArray(arr,n);
-        if(isSorted(arr)) {
+        int[] arr = ArrayUtility.inputArray();
+        if(isAscending(arr) || isDescending(arr)) {
             System.out.println("Array is sorted..");
         }
         else {
             System.out.println("Array is not sorted..");
         }
-        sc.close();
+        ArrayUtility.sc.close();
+    }
+    // is array sorted in ascending manner
+    public static boolean isAscending(int[] arr)
+    {
+        int i=1;
+        while(i< arr.length)
+        {
+            if(arr[i]<arr[i-1])
+                return false;
+            i++;
+        }
+        return true;
+    }
+    // is array sorted in descending manner
+    public static boolean isDescending(int[] arr)
+    {
+        int i=1;
+        while(i< arr.length)
+        {
+            if(arr[i]>arr[i-1])
+                return false;
+            i++;
+        }
+        return true;
     }
 }
 
