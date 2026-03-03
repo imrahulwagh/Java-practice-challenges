0c76500d0eb0989474ae4f07ad55974450f40b98
Rahul Wagh <143119467+imrahulwagh@users.noreply.github.com>
2026-03-03 23:08:38 +0530

    Refactor array input handling in CH42.java
    Refactored CH42.java to use ArrayUtility for input.

--- src/CH42.java
+++ src/CH42.java
@@ -1,17 +1,6 @@
 // Create a program to find the maximum and minimum element in an array.
 import java.util.Scanner;
 public class CH42 {
-    static Scanner sc = new Scanner(System.in);
-    public static void inputArray(int[] arr, int n)
-    {
-        System.out.print("Enter "+n+" elements : ");
-        int i=0,count= 0;
-        while(i<arr.length)
-        {
-            arr[i] = sc.nextInt();
-            i++;
-        }
-    }
     // Maximum from Array
     public static int max(int[] arr)
     {
@@ -40,13 +29,10 @@ public class CH42 {
         return minNumber;
     }
     public static void main(String[] args) {
-        System.out.print("Enter a number : ");
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        inputArray(arr,n);
+        int[] arr = ArrayUtility.inputArray();
         System.out.println("Maximum is "+max(arr));
         System.out.println("Minimum is "+min(arr));
-        sc.close();
+        ArrayUtility.sc.close();
     }
 }
 
