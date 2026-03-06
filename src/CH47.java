ccccbca693f56a582a66bdbbe0f54e2efc6f0ed5
Rahul Wagh <waghrahul686@gmail.com>
2026-03-07 00:54:37 +0530

    Added question statement as comment in the first line

--- src/CH47.java
+++ src/CH47.java
@@ -1,3 +1,5 @@
+// 47. Create a program to merge two sorted arrays.
+import java.util.Scanner;
 public class CH47 {
     public static void main(String[] args) {
         System.out.println("Hello CH47");
