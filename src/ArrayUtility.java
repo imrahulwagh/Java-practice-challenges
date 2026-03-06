fd9e7533482de1fcf971634634b6c6485dfdfd85
Rahul Wagh <waghrahul686@gmail.com>
2026-03-07 01:10:47 +0530

    used enhanced for-each loop

--- src/ArrayUtility.java
+++ src/ArrayUtility.java
@@ -14,9 +14,8 @@ class ArrayUtility {
     }
     public static void displayArray(int[] arr)
     {
-        for(int i=0;i<arr.length;i++)
-        {
-            System.out.print(arr[i]+" ");
+        for (int j : arr) {
+            System.out.print(j + " ");
         }
     }
 }
