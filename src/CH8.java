bd9b36d1a0a97f128c5a33423ea1bc273ab75c9b
Rahul Wagh <143119467+imrahulwagh@users.noreply.github.com>
2026-03-13 10:58:48 +0530

    Update prompts for user input in CH8.java

--- src/CH8.java
+++ src/CH8.java
@@ -1,12 +1,12 @@
-//8.Create a program that takes two numbers and shows result of all
-//arithmetic operators (+,-,*,/,%).
+//8.Create a program that takes two numbers and shows result of all arithmetic operators (+,-,*,/,%).
 import java.util.Scanner;
 public class CH8
 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter two numbers : ");
+        System.out.print("Enter first number : ");
         int a = sc.nextInt();
+        System.out.print("Enter second number : ");
         int b = sc.nextInt();
         System.out.println(a+" + "+b+" = "+(a+b));
         System.out.println(a+" - "+b+" = "+(a-b));
