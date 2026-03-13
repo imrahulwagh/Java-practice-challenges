3eea0c121d24253068922ac9e419493676fc78c7
Rahul Wagh <143119467+imrahulwagh@users.noreply.github.com>
2026-03-13 11:19:56 +0530

    Change Simple Interest calculation to use float

--- src/CH12.java
+++ src/CH12.java
@@ -12,7 +12,7 @@ public class CH12
         float t = sc.nextFloat();
         System.out.print("Enter the Rate of Interest in percentage : ");
         float r = sc.nextFloat();
-        double si = (p*t*r)/100;
+        float si = (p*t*r)/100;
         System.out.print("\nThe Simple Interest is "+si);
         sc.close();
     }
