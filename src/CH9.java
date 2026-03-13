6935626461086c1e79c8e9eafa4140e4a1e1d691
Rahul Wagh <143119467+imrahulwagh@users.noreply.github.com>
2026-03-13 11:05:49 +0530

    Update prompts and output messages in CH9.java

--- src/CH9.java
+++ src/CH9.java
@@ -4,10 +4,11 @@ public class CH9
 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter two floating numbers : ");
+        System.out.print("Enter first floating number : ");
         double a = sc.nextDouble();
+        System.out.print("Enter second floating number : ");
         double b = sc.nextDouble();
-        System.out.println("The product of "+a+" and "+b+" is "+(a*b));
+        System.out.println("The multiplication of "+a+" and "+b+" is "+(a*b));
         sc.close();
     }
 }
