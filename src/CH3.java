5b92188f937a644010622f64572fdee4e2aeaf7d
Rahul Wagh <143119467+imrahulwagh@users.noreply.github.com>
2026-03-13 10:43:48 +0530

    Refactor CH3.java to print star patterns

--- src/CH3.java
+++ src/CH3.java
@@ -1,12 +1,23 @@
-public class CH3 {
-    public static void main(String[] args)
-    {
-        for(int i=0;i<5;i++)
-        {
-            for(int j=0;j<=i;j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
+class CH3 {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+        System.out.println("--------------");
+
+        System.out.println("* * * * *");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+        System.out.println("--------------");
+
+        System.out.println("        *");
+        System.out.println("      * *");
+        System.out.println("    * * *");
+        System.out.println("  * * * *");
+        System.out.println("* * * * *");
     }
 }
