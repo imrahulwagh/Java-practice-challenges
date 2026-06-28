73dc1eb27dbd7341dc84574c02f7f69efe523ef9
Rahul Wagh <waghrahul686@gmail.com>
2026-06-29 02:41:37 +0530

    Challenge 51 revised

--- src/CH51.java
+++ src/CH51.java
@@ -1,58 +1,62 @@
+/* 51. Create a Book class for a library system.
+• Instance variables: title, author, isbn.
+• Static variable: totalBooks, a counter for the total number of book instances.
+• Instance methods: borrowBook(), returnBook().
+• Static method: getTotalBooks(), to get the total number of books in the library. */
+
 class Book {
-    static int totalBooks;
-    String title;
-    String author;
-    String isbn;
-
+    static int totalNoOfBooks;
+    String title, author, isbn ;
     boolean isBorrowed;
+
+    // Static block
     static {
-        totalBooks = 0;
-    }
-    { // Object Init
-        totalBooks++;
+        totalNoOfBooks = 0;
     }
 
-    Book(String isbn,String title,String author){
-        this.isbn = isbn;
-        this.title = title;
-        this.author = author;
+    // Initialization block
+    {
+        totalNoOfBooks++;
+    }
+
+    Book(String isbn,String title,String author) {
+        this.isbn = isbn ;
+        this.title = title ;
+        this.author = author ;
     }
 
     Book(String isbn){
         this(isbn,"Unknown","Unknown");
     }
 
-    static int getTotalBooks() {
-        return totalBooks;
+    static int getTotalBooks(){
+        return totalNoOfBooks;
     }
 
-    void borrowBook() {
+    public void borrowBook(){
         if(isBorrowed){
-            System.out.println("Book has already borrowed");
-        } else{
-            this.isBorrowed = true;
-            System.out.println("Enjoy "+this.title);
+            System.out.println("Book is already borrowed");
+        }else{
+            isBorrowed = true;
+            System.out.println("Enjoy your book");
         }
     }
-
-    void returnBook() {
+    public void returnBook(){
         if(isBorrowed){
-            this.isBorrowed = false;
-            System.out.println("Hope you enjoyed the book.\nPlease leave a review");
-        } else {
-            System.out.println("This book is already in the library");
+            isBorrowed = false;
+            System.out.println("Hope you enjoyed the book \nPlease leave the review");
+        }
+        else{
+            System.out.println("The book is already in the library");
         }
     }
-
 
     public static void main(String[] args) {
-        Book IOT = new Book("1","Internet of Things","Ramanujan");
-        Book myBook = new Book("2");
-        System.out.println(Book.getTotalBooks());
-        IOT.borrowBook();
-        myBook.borrowBook();
-        IOT.borrowBook();
-        IOT.returnBook();
-        IOT.returnBook();
+        System.out.println("No. of books in the library : "+getTotalBooks());
+        Book java = new Book("123","java","James Gangling");
+        java.borrowBook();
+        java.borrowBook();
+        System.out.println("No. of books in the library : "+getTotalBooks());
+        Book python = new Book("124");
     }
 }
