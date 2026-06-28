f5dca28288caba18d8f3958c79219cf84c9a2d76
Rahul Wagh <waghrahul686@gmail.com>
2026-06-29 03:30:12 +0530

    Challenge 93 done

---
+++ src/CH93.java
@@ -0,0 +1,30 @@
+// ///////////////////////////////////////////////////////////////////
+//
+//  Function name : reverseList
+//  Input :         List
+//  Output :        List
+//  Description :   Accepts a list and uses reverseList
+//                  function to reverse the list using the
+//                  swapping logic used in previous challenge.
+//  Date :          29/05/2026
+//  Author :        Rahul Vilas Wagh
+//
+// ///////////////////////////////////////////////////////////////////
+
+import java.util.Arrays;
+import java.util.List;
+class CH93 {
+    public static void main(String[] args) {
+        List<Integer> numList = Arrays.asList(11,22,33,44,55);
+        System.out.println("Before reversal..");
+        System.out.println(numList);
+        reverseList(numList);
+        System.out.println("After reversal..");
+        System.out.println(numList);
+    }
+    public static void reverseList(List<Integer> list){
+        for(int i=0;i<list.size()/2;i++){
+            Swapping.swapUsingIndex(list,i,list.size() - 1 - i);
+        }
+    }
+}
